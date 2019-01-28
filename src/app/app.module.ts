0feb18e0a0526126e5cb8e0d95a54925a7585941
anast29 <ivanovaanast29@gmail.com>
2019-01-28 12:06:10 +0200

    ONAFTLibrary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {RouterModule, Routes} from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
+const rout: Routes = [];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      RouterModule.forRoot(rout)
   ],
   providers: [],
   bootstrap: [AppComponent]
