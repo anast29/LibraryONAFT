395dfd5db3c54950fa99f3021917482310ce3750
anast29 <ivanovaanast29@gmail.com>
2019-04-08 17:48:49 +0300

    ONAFTLibrary

--- src/app/jubilee/jubilee.component.ts
+++ src/app/jubilee/jubilee.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Scientists} from '../scientists';
+import {HttpService} from '../http.service';
 
 @Component({
-  selector: 'app-jubilee',
-  templateUrl: './jubilee.component.html',
-  styleUrls: ['./jubilee.component.css']
+    selector: 'app-jubilee',
+    templateUrl: './jubilee.component.html',
+    styleUrls: ['./jubilee.component.css']
 })
 export class JubileeComponent implements OnInit {
+    scientists: Scientists;
 
-  constructor() { }
+    constructor(private httpservice: HttpService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.httpservice.get('http://192.168.1.39:8000/scientists/').subscribe((data: Scientists) => this.scientists = data);
+
+    }
+
+    searchDate(date) {
+        const TODAY = new Date();
+        const NEW_DATE = TODAY.getFullYear() - Number(date);
+        console.log(NEW_DATE);
+        return NEW_DATE;
+    }
 
 }
